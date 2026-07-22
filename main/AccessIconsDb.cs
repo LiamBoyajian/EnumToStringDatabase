145deed61b5891bbb753e7183334b9b4071cbc69
Liam Boyajian <thefirstboyajian@gmail.com>
2026-07-21 19:23:40 -0500

    add: methods and tests in EnumToIcon

--- main/AccessIconsDb.cs
+++ main/AccessIconsDb.cs
@@ -1,37 +1,148 @@
 using System;
 using System.IO;
+using Dapper;
+using Main.main.scripts.core.plants;
 using Microsoft.Data.Sqlite;
 
 namespace Main.addons.EnumToIcon.main;
 
-public static class AccessIconsDb
+public struct Entry(Enum @enum, int size, string path = "")
 {
-    public const string DataSource = "Data Source=";
+    public string Path { get; set; } = path;
+    public int Size { get; set; } = size;
+    public Enum @Enum { get; set; } = @enum;
 
-    public static string DbPath =>
+    public int GetOrdinal()
+    {
+        return Convert.ToInt32(@Enum);
+    }
+
+    public string GetEnumName()
+    {
+        return @Enum.GetType().Name;
+    }
+
+    public void GenVariables(out Enum @enum, out int size, out string data)
+    {
+        @enum = @Enum;
+        size = Size;
+        data = Path;
+    }
+}
+
+public class AccessIconsDb
+{
+    public static string DbPath { get; set; } =
         Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "addons/EnumToIcon/main/enum_to_directory.db");
 
-    public static string GetFile(Enum @enum, int size)
+    public AccessIconsDb(string path = null)
     {
-        using var connection = new SqliteConnection(DataSource + DbPath);
+        if (path != null)
+            DbPath = path;
+    }
+
+    public static string GetFileAddress(Enum @enum, int size = -1)
+    {
+        using var connection = new SqliteConnection($"Data Source={DbPath};");
+        connection.Open();
+        var reader = _iconEntries(@enum, connection, size).ExecuteReader();
+        if (reader is not { HasRows: true })
+            return null;
+
+        reader.Read();
+
+        return reader.GetString(reader.GetOrdinal("Path"));
+    }
+
+    /**
+     *
+     */
+    public static bool PutIcon(Entry entry)
+    {
+        entry.GenVariables(out var @enum, out var size, out var data);
+
+        if (size < 0) return false;
+
+        using var connection = new SqliteConnection($"Data Source={DbPath};");
+        connection.Open();
+        if (connection == null) throw new Exception("Established connection was not found");
+
+        using var commandValueEnum = connection.CreateCommand(); //Reusing old connection
+
+        commandValueEnum.CommandText = """
+                                       INSERT INTO ValueEnum (ParentEnum, Value)
+                                       VALUES (@enum, @ordinal);
+                                       SELECT last_insert_rowid(); 
+                                       """;
+        commandValueEnum.Parameters.Add(new SqliteParameter("@enum", @enum.GetType().Name));
+        commandValueEnum.Parameters.Add(new SqliteParameter("@ordinal", entry.GetOrdinal()));
+        var newRowKey = Convert.ToInt32(commandValueEnum.ExecuteScalar() ?? -1);
+
+        if (newRowKey < 0) throw new Exception("ValueEnum row was not created");
+
+        using var commandIdToFile = connection.CreateCommand();
+
+        commandIdToFile.CommandText = """
+                                      INSERT INTO IdToFile (Size, ParentKey, Path)
+                                      VALUES (@size, @parentKey, @path);
+                                      SELECT last_insert_rowid();
+                                      """;
+        commandIdToFile.Parameters.Add(new SqliteParameter("@size", size));
+        commandIdToFile.Parameters.Add(new SqliteParameter("@parentKey", newRowKey));
+        commandIdToFile.Parameters.Add(new SqliteParameter("@path", data));
+
+        newRowKey = Convert.ToInt32(commandIdToFile.ExecuteScalar() ?? -1);
+        if (newRowKey < 0) throw new Exception("IdToFile row was not created");
+
+        return true;
+    }
+
+    private static bool UpdateIcon(Entry entry)
+    {
+        throw new NotImplementedException();
+        return false;
+    }
+
+    public static int IconEntryCount(Enum @enum, int size = -1)
+    {
+        using var connection = new SqliteConnection($"Data Source={DbPath};");
+        connection.Open();
+        var reader = _iconEntries(@enum, connection, size).ExecuteReader();
+        var result = 0;
+        while (reader.HasRows)
+        {
+            reader.Read();
+            result += 1;
+        }
+
+        return result;
+    }
+
+    //PRIVATE HELPER METHODS
+
+    private static SqliteCommand _iconEntries(Enum @enum, SqliteConnection connection, int size = -1)
+    {
+        if (connection == null)
+            throw new ArgumentException("connection was null");
+
         connection.Open();
 
-        string enumName = @enum.GetType().Name;
+        if (@enum == null) return null;
+        var enumOrdinal = Convert.ToInt32(@enum);
 
-        using var command = connection.CreateCommand();
+        var command = connection.CreateCommand();
 
-        command.Parameters.Add(new SqliteParameter("@size", size));
-        command.Parameters.Add(new SqliteParameter("@enumName", enumName));
-        Console.WriteLine(enumName);
         command.CommandText = """
-                              SELECT IdToFile.Path, * 
-                              FROM IdToFile 
+                              SELECT *
+                              FROM IdToFile
                               JOIN ValueEnum ON IdToFile.ParentKey = ValueEnum.Key
-                              WHERE IdToFile.Id = @size AND ValueEnum.ParentEnum = @enumName;
+                              WHERE ValueEnum.ParentEnum = @enum AND ValueEnum.Value = @enumOrdinal AND (@size < 0 OR IdToFile.Size = @size); 
                               """;
-        using var reader = command.ExecuteReader();
-        if (reader.Read())
-            return reader.GetString(0);
-        return string.Empty;
+
+        command.Parameters.Add(new SqliteParameter("@enum", @enum.GetType().Name));
+        command.Parameters.Add(new SqliteParameter("@enumOrdinal", enumOrdinal));
+        command.Parameters.Add(new SqliteParameter("@size", size));
+
+        return command;
     }
 }
--- tests/AccessIconsDbTest.cs
+++ tests/AccessIconsDbTest.cs
@@ -1,6 +1,8 @@
 using System;
+using System.IO;
 using Main.addons.EnumToIcon.main;
 using Main.main.scripts.core.plants;
+using Microsoft.Data.Sqlite;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Main.addons.EnumToIcon.tests;
@@ -8,10 +10,78 @@ namespace Main.addons.EnumToIcon.tests;
 [TestClass]
 public class AccessIconsDbTest
 {
-    [TestMethod]
-    public void TestGetIcon()
+    private static string _tempDbPath;
+    private static SqliteConnection _connection;
+
+    [ClassInitialize]
+    public static void ClassInitialize(TestContext context)
     {
-        //Console.WriteLine(AbstractPlant.Rt.Health.GetType().Name);
-        Assert.AreEqual("AGA", AccessIconsDb.GetFile(AbstractPlant.Rt.Health, 32));
+        _tempDbPath = Path.Combine(Path.GetTempPath(), Path.GetRandomFileName());
+        _connection = new SqliteConnection($"Data Source={_tempDbPath};");
+        _connection.Open();
+
+        using var command = _connection.CreateCommand();
+        string scriptPath = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "addons", "EnumToIcon", "main",
+            "db_declaration.sql");
+        command.CommandText = File.ReadAllText(scriptPath);
+        command.ExecuteNonQuery();
+
+        AccessIconsDb.DbPath = _tempDbPath;
+    }
+
+    [ClassCleanup]
+    public static void ClassCleanup()
+    {
+        _connection?.Close();
+        _connection?.Dispose();
+        SqliteConnection.ClearAllPools();
+        if (File.Exists(_tempDbPath))
+            File.Delete(_tempDbPath);
+    }
+
+    [TestMethod]
+    public void TestPut()
+    {
+        Assert.AreEqual(true, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, 16, "EXAMPLE")));
+    }
+
+    [TestMethod]
+    public void TestGetFile()
+    {
+        Assert.AreEqual(true, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, 16, "EXAMPLE")));
+
+        Assert.AreEqual("EXAMPLE", AccessIconsDb.GetFileAddress(AbstractPlant.Rt.Health, 16));
+    }
+
+
+    [TestMethod]
+    public void TestPutConfirmValue()
+    {
+        Assert.AreEqual(true, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, 16, "EXAMPLE")));
+
+        Assert.AreEqual("EXAMPLE", AccessIconsDb.GetFileAddress(AbstractPlant.Rt.Health, 16));
+
+        Assert.AreEqual(true, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, 16, "FORSEN")));
+
+        Assert.AreEqual("FORSEN", AccessIconsDb.GetFileAddress(AbstractPlant.Rt.Health, 16));
+    }
+
+    [TestMethod]
+    public void TestPutNull()
+    {
+        Assert.AreEqual(true, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, 64, null)));
+    }
+
+    [TestMethod]
+    public void TestPutBadInt()
+    {
+        Assert.AreEqual(false, AccessIconsDb.PutIcon(new Entry(AbstractPlant.Rt.Health, -1, "EXAMPLE")));
+    }
+
+    // TEST ENTRYCOUNT
+    [TestMethod]
+    public void Test()
+    {
+        //Assert.AreEqual(2, AccessIconsDb.IconEntryCount(AbstractPlant.Rt.Health));
     }
 }
